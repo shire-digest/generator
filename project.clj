b2ce0c3b3511d328e4a4f2259cc12866f4cb4c31
hbc <bcxxxxxx@gmail.com>
2014-10-27 09:12:19 +0800

    fix: dependency required.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
             :url "https://github.com/xhacker/SMPPL/blob/master/SMPPL-Freeware.md"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [de.ubercode.clostache/clostache "1.4.0"]]
+                 [de.ubercode.clostache/clostache "1.4.0"]
+                 [shire-digest.meta "0.0.1"]]
   
   :source-paths ["src"])
